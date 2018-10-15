139493787ef93e272eed5abad7d2c6c36a8bfeec
Alvin Dizon <alvin.dizon@ghl.com>
2018-10-15 10:55:02 +0800

    Display response from stackoverflow API
    Signed-off-by: Alvin Dizon <alvin.dizon@ghl.com>

--- app/src/main/java/com/chikeandroid/retrofittutorial/MainActivity.java
+++ app/src/main/java/com/chikeandroid/retrofittutorial/MainActivity.java
@@ -2,12 +2,85 @@ package com.chikeandroid.retrofittutorial;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.chikeandroid.retrofittutorial.adapter.AnswersAdapter;
+import com.chikeandroid.retrofittutorial.data.model.Item;
+import com.chikeandroid.retrofittutorial.data.model.SOAnswersResponse;
+import com.chikeandroid.retrofittutorial.data.remote.ApiUtils;
+import com.chikeandroid.retrofittutorial.data.remote.SOService;
+
+import java.util.ArrayList;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+import static com.chikeandroid.retrofittutorial.data.remote.ApiUtils.*;
 
 public class MainActivity extends AppCompatActivity {
+
+    private AnswersAdapter mAdapter;
+    private RecyclerView mRecyclerView;
+    private SOService mService;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mService = getSOService();
+        mRecyclerView = (RecyclerView) findViewById(R.id.rv_answers);
+        mAdapter = new AnswersAdapter(this, new ArrayList<Item>(0), new AnswersAdapter.PostItemListener() {
+
+            @Override
+            public void onPostClick(long id) {
+                Toast.makeText(MainActivity.this, "Post id is" + id, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(layoutManager);
+        mRecyclerView.setAdapter(mAdapter);
+        mRecyclerView.setHasFixedSize(true);
+        RecyclerView.ItemDecoration itemDecoration = new DividerItemDecoration(this, DividerItemDecoration.VERTICAL_LIST);
+        mRecyclerView.addItemDecoration(itemDecoration);
+
+        loadAnswers();
+    }
+
+    public void loadAnswers() {
+
+//        enqueue() asynchronously sends the request and notifies your app with a callback
+//        when a response comes back. Since this request is asynchronous,
+//        Retrofit handles it on a background thread so that the main UI thread
+//        isn't blocked or interfered with.
+        mService.getAnswers().enqueue(new Callback<SOAnswersResponse>() {
+//            To use enqueue(), you have to implement two callback methods: onResponse and onFailure
+            @Override
+            public void onResponse(Call<SOAnswersResponse> call, Response<SOAnswersResponse> response) {
+
+                if(response.isSuccessful()) {
+                    mAdapter.updateAnswers(response.body().getItems());
+                    Log.d("MainActivity", "posts loaded from API");
+                }else {
+                    int statusCode  = response.code();
+                    // handle request errors depending on status code
+                }
+            }
+
+            @Override
+            public void onFailure(Call<SOAnswersResponse> call, Throwable t) {
+                showErrorMessage();
+                Log.d("MainActivity", "error loading from API");
+
+            }
+        });
+    }
+
+    public void showErrorMessage() {
+        Toast.makeText(this, "Error loading posts", Toast.LENGTH_SHORT).show();
     }
 }
