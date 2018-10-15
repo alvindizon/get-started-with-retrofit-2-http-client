1d97be2379aa718e72fa7d1df9fc6d05747c364d
Alvin Dizon <alvin.dizon@ghl.com>
2018-10-15 11:16:28 +0800

    Update HTTP request methods to return Observables
    Signed-off-by: Alvin Dizon <alvin.dizon@ghl.com>

--- app/src/main/java/com/chikeandroid/retrofittutorial/data/remote/SOService.java
+++ app/src/main/java/com/chikeandroid/retrofittutorial/data/remote/SOService.java
@@ -5,12 +5,13 @@ import com.chikeandroid.retrofittutorial.data.model.SOAnswersResponse;
 import retrofit2.Call;
 import retrofit2.http.GET;
 import retrofit2.http.Query;
+import rx.Observable;
 
 public interface SOService {
 
     @GET("/answers?order=desc&sort=activity&site=stackoverflow")
-    Call<SOAnswersResponse> getAnswers();
+    Observable<SOAnswersResponse> getAnswers();
 
     @GET("/answers?order=desc&sort=activity&site=stackoverflow")
-    Call<SOAnswersResponse> getAnswers(@Query("tagged") String tags);
+    Observable<SOAnswersResponse> getAnswers(@Query("tagged") String tags);
 }
