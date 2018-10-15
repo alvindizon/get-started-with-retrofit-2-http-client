c698e9d53ab6f3d92827aa4ff5daf2ae93b58ea8
Alvin Dizon <alvin.dizon@ghl.com>
2018-10-15 11:15:32 +0800

    Add RxJava CallAdapter on building of Retrofit instance
    Signed-off-by: Alvin Dizon <alvin.dizon@ghl.com>

--- app/src/main/java/com/chikeandroid/retrofittutorial/data/remote/RetrofitClient.java
+++ app/src/main/java/com/chikeandroid/retrofittutorial/data/remote/RetrofitClient.java
@@ -1,6 +1,7 @@
 package com.chikeandroid.retrofittutorial.data.remote;
 
 import retrofit2.Retrofit;
+import retrofit2.adapter.rxjava.RxJavaCallAdapterFactory;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class RetrofitClient {
@@ -12,6 +13,7 @@ public class RetrofitClient {
             retrofit = new Retrofit.Builder()
                     .baseUrl(baseUrl)
                     .addConverterFactory(GsonConverterFactory.create())
+                    .addCallAdapterFactory(RxJavaCallAdapterFactory.create())
                     .build();
         }
         return retrofit;
