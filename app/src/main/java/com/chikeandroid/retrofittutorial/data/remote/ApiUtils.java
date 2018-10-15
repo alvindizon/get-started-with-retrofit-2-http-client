beea5cfe0b682d67b527dca95faeffab26ed635b
Alvin Dizon <alvin.dizon@ghl.com>
2018-10-15 10:25:05 +0800

    Added class that contains BASE_URL and provides the interface to the app
    Signed-off-by: Alvin Dizon <alvin.dizon@ghl.com>

---
+++ app/src/main/java/com/chikeandroid/retrofittutorial/data/remote/ApiUtils.java
@@ -0,0 +1,11 @@
+package com.chikeandroid.retrofittutorial.data.remote;
+
+public class ApiUtils {
+
+    public static final String BASE_URL = "https://api.stackexchange.com/2.2/";
+
+    public static SOService getSOService() {
+        return RetrofitClient.getClient(BASE_URL).create(SOService.class);
+    }
+
+}
